708a9de7ef67aeeb37e2993290351f173fae6caf
mca181 <mca181@sfu.ca>
2021-11-13 14:16:47 -0800

    Started creating JFrame and JPanel under Game Class and GameBoard Class

---
+++ Blob2DGame/src/main/java/com/BlobGame/GameBoard.java
@@ -0,0 +1,11 @@
+package com.BlobGame;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.*;
+
+public class GameBoard extends JPanel implements ActionListener, KeyListener {
+	
+}
