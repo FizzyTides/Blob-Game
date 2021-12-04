81faed846f1cc8ecfc703250a36c0bdb59d8509c
mca181 <mca181@sfu.ca>
2021-12-03 19:01:32 -0800

    Fixed test CheckWalls

--- Blob2DGame/src/test/java/com/BlobGame/GameBoardTest.java
+++ Blob2DGame/src/test/java/com/BlobGame/GameBoardTest.java
@@ -104,44 +104,49 @@ class GameBoardTest {
             	testPlayer.wallNorth = true;
                 aWallNorth = true;
                 Assertions.assertTrue(aWallNorth, "There is no wall North");
+                Assertions.assertTrue(testPlayer.wallNorth, "There is no wall North");
                 System.out.println("There is a wall North!");
             }
             else {
             	testPlayer.wallNorth = false;
             	Assertions.assertFalse(testPlayer.wallNorth);
-            	System.out.println("There is no wall North!");
+            	System.out.println("There is not a wall North!");
             }
             if((testPlayer.pos.y + 1 == wallCurrPos.y && testPlayer.pos.x == wallCurrPos.x) || aWallSouth) {
             	testPlayer.wallSouth = true;
                 aWallSouth = true;
                 Assertions.assertTrue(aWallSouth, "There is no wall South");
+                Assertions.assertTrue(testPlayer.wallSouth, "There is no wall South");
                 System.out.println("There is a wall South!");
             }
             else {
             	testPlayer.wallSouth = false;
             	Assertions.assertFalse(testPlayer.wallSouth);
-            	System.out.println("There is a wall Soth!");
+            	System.out.println("There is not a wall South!");
             }
-            if((testPlayer.pos.x - 1== wallCurrPos.x &&testPlayer.pos.y == wallCurrPos.y) || aWallEast) {
+            if((testPlayer.pos.x + 1 == wallCurrPos.x &&testPlayer.pos.y == wallCurrPos.y) || aWallEast) {
             	testPlayer.wallEast = true;
                 aWallEast = true;
                 Assertions.assertTrue(aWallEast, "There is no wall East");
+                Assertions.assertTrue(testPlayer.wallEast, "There is no wall East");
                 System.out.println("There is a wall East!");
             }
             else {
             	testPlayer.wallEast = false;
-            	System.out.println("There is a wall East!");
+            	Assertions.assertFalse(testPlayer.wallEast);
+            	System.out.println("There is not a wall East!");
             }
-            if((testPlayer.pos.x + 1 == wallCurrPos.x && testPlayer.pos.y == wallCurrPos.y) || aWallWest) {
+            if((testPlayer.pos.x - 1 == wallCurrPos.x && testPlayer.pos.y == wallCurrPos.y) || aWallWest) {
             	testPlayer.wallWest = true;
                 aWallWest = true;
                 Assertions.assertTrue(aWallWest, "There is no wall West");
+                Assertions.assertTrue(testPlayer.wallWest, "There is no wall West");
                 System.out.println("There is a wall West!");
             }
             else {
             	testPlayer.wallWest = false;
             	Assertions.assertFalse(testPlayer.wallWest, "This should be false!");
-            	System.out.println("There is a wall West!");
+            	System.out.println("There is not a wall West!");
             }
         }
 	}
