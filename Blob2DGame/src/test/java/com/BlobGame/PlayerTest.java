9a77a2c4c2006b3257d07aa243dbd8d346151213
mca181 <mca181@sfu.ca>
2021-12-03 18:43:40 -0800

    Modified playerTest frozentimer working correctly now

--- Blob2DGame/src/test/java/com/BlobGame/PlayerTest.java
+++ Blob2DGame/src/test/java/com/BlobGame/PlayerTest.java
@@ -8,21 +8,24 @@ import org.junit.jupiter.api.Test;
 class PlayerTest {
 
 	@Test
-	void testFrozenCheck() {
+	void testFrozenTimer() {
+		int timeElapsed = 4;
 		//Start off frozen for the frozen timer duration
 		boolean frozenCheck = true;
-		int punishmentFreezeTime = 1000; // Freeze duration
+		int punishmentFreezeTime = 5; // Freeze duration
 		long frozenStartTime = -punishmentFreezeTime; // In actual game, frozenStartTime gets marked upon hitting punishment
 		frozenStartTime = 0; // Player gets frozen at the start of the game
 		//If the current time passed surpasses the time frozen + duration of freeze, melt
-		if(frozenStartTime + punishmentFreezeTime > System.currentTimeMillis()) {
-			System.out.println("Still Frozen!");
+		if(frozenStartTime + punishmentFreezeTime > timeElapsed) {
+			System.out.println("Frozen Time not met, FROZEN!");
 			frozenCheck = true;
-			
-		}
-		else {
-			System.out.println("Unfrozen!");
-			frozenCheck = false;
+			Assertions.assertTrue(frozenCheck);
+			//Time passed 5 seconds simulated, actual using System.millis
+			timeElapsed += 5;
+			if(timeElapsed > frozenStartTime + punishmentFreezeTime) {
+				frozenCheck = false;
+				System.out.println("Frozen Time exceeded! UNFREEZING!");
+			}
 		}
 		//Asserting false as we want to melt once the set duration has passed currently set to 1000 ms
 		Assertions.assertFalse(frozenCheck, "STILL FROZEN!");
