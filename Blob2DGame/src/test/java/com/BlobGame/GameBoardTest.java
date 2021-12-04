573990a105bf713b9680ad3ae302a7fc8ded835a
mca181 <mca181@sfu.ca>
2021-12-03 18:42:21 -0800

    Added test checkwalls integration test

--- Blob2DGame/src/test/java/com/BlobGame/GameBoardTest.java
+++ Blob2DGame/src/test/java/com/BlobGame/GameBoardTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.awt.Point;
 import java.time.Duration;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
@@ -70,20 +72,79 @@ class GameBoardTest {
 		Assertions.assertTrue(playerHit, "Player did not hit Punishment Tile");
 	}
 	
-	/*@Test
-	public void testBonusVisibility() {
-		System.out.println("Testing BonusVisibility()");
-		for (Cake cake : rewards) {
-			if((cake.isBonus == true) && (gameTimeElapsed == rand)) {
-				cake.setBonusReward((int) (player.getScore() * 1.5 + 200));
-				cake.visibility = true; 
-			}
-			if((cake.isBonus == true) && (gameTimeElapsed == (rand + bonusStayTime)) && cake.visibility == true) {
-				cake.visibility = false; 
-			}
-		}
+	@Test
+	void testCheckWalls() {
+		System.out.println("Testing CheckWalls!");
+		ArrayList<Wall> testWalls = new ArrayList<Wall>();
+		boolean aWallNorth, aWallSouth, aWallWest, aWallEast;
+		aWallNorth = aWallSouth = aWallWest = aWallEast = false;
+		Player testPlayer = new Player();
+		Point origin = new Point(1,1);
+		testPlayer.setPos(origin);
+		Assertions.assertEquals(origin, testPlayer.getPos(), "Player did not spawn at origin");
 		
-	}*/
+		Wall wallN = new Wall(new Point(1,0));
+		Wall wallS = new Wall(new Point(1,2));
+		Wall wallE = new Wall(new Point(2,1));
+		Wall wallW = new Wall(new Point(0,2));
+		
+		testWalls.add(wallN);
+		testWalls.add(wallS);
+		testWalls.add(wallE);
+		testWalls.add(wallW);
+		
+		//If it works for one wall should work for all walls
+		Assertions.assertEquals(new Point(1,0), wallN.getPos(), "WallNorth spawned incorrectly!");
+		
+        for(Wall walls : testWalls) {
+            Point wallCurrPos = walls.getPos();
+            
+
+            if((testPlayer.pos.y - 1 == wallCurrPos.y && testPlayer.pos.x == wallCurrPos.x) || aWallNorth) {
+            	testPlayer.wallNorth = true;
+                aWallNorth = true;
+                Assertions.assertTrue(aWallNorth, "There is no wall North");
+                System.out.println("There is a wall North!");
+            }
+            else {
+            	testPlayer.wallNorth = false;
+            	Assertions.assertFalse(testPlayer.wallNorth);
+            	System.out.println("There is no wall North!");
+            }
+            if((testPlayer.pos.y + 1 == wallCurrPos.y && testPlayer.pos.x == wallCurrPos.x) || aWallSouth) {
+            	testPlayer.wallSouth = true;
+                aWallSouth = true;
+                Assertions.assertTrue(aWallSouth, "There is no wall South");
+                System.out.println("There is a wall South!");
+            }
+            else {
+            	testPlayer.wallSouth = false;
+            	Assertions.assertFalse(testPlayer.wallSouth);
+            	System.out.println("There is a wall Soth!");
+            }
+            if((testPlayer.pos.x - 1== wallCurrPos.x &&testPlayer.pos.y == wallCurrPos.y) || aWallEast) {
+            	testPlayer.wallEast = true;
+                aWallEast = true;
+                Assertions.assertTrue(aWallEast, "There is no wall East");
+                System.out.println("There is a wall East!");
+            }
+            else {
+            	testPlayer.wallEast = false;
+            	System.out.println("There is a wall East!");
+            }
+            if((testPlayer.pos.x + 1 == wallCurrPos.x && testPlayer.pos.y == wallCurrPos.y) || aWallWest) {
+            	testPlayer.wallWest = true;
+                aWallWest = true;
+                Assertions.assertTrue(aWallWest, "There is no wall West");
+                System.out.println("There is a wall West!");
+            }
+            else {
+            	testPlayer.wallWest = false;
+            	Assertions.assertFalse(testPlayer.wallWest, "This should be false!");
+            	System.out.println("There is a wall West!");
+            }
+        }
+	}
 	
 	@Test
 	void testBonusVisibility() {
